0a2e11af5ee22c4c79becf9dbe83bc44e9e9e874
赖云傑(Lai Yunjie) <wolkej@qq.com>
2026-03-17 16:50:34 +0800

    Update repository URL for robot descriptions

--- scripts/download_robot_descriptions.sh
+++ scripts/download_robot_descriptions.sh
@@ -28,7 +28,7 @@ fi
 ROBOT_DESC_DIR="${PROJECT_ROOT}/${TARGET_DIR}"
 
 # Repository configuration
-REPO_URL="https://github.com/fan-ziqi/rl_sar_zoo.git"
+REPO_URL="https://github.com/wolkej377/rl_sar_zoo.git"
 REPO_BRANCH="main"
 
 # Expected version - update this when URDF files need to be updated
