249b656b424e5cc8ad4c8222cbfcb4d75163483b
赖云傑(Lai Yunjie) <wolkej@qq.com>
2026-03-20 17:04:36 +0800

    Update fsm_go2.hpp
    default config:parkour

--- src/rl_sar/fsm_robot/fsm_go2.hpp
+++ src/rl_sar/fsm_robot/fsm_go2.hpp
@@ -162,7 +162,7 @@ public:
         rl.episode_length_buf = 0;
 
         // read params from yaml
-        rl.config_name = "himloco";
+        rl.config_name = "parkour";
         std::string robot_config_path = rl.robot_name + "/" + rl.config_name;
         try
         {
